898915967d5770174df6c8c325025d52da926ea1
Matheus <matheus_romeike@sisand.com.br>
2023-08-25 15:45:47 -0300

    ajustando classes

---
+++ src/FloodFill.java
@@ -0,0 +1,13 @@
+public class FloodFill {
+    private int[][] imagem;
+    private int novaCor = 2;
+    private static int corDeFundo = 0;
+
+    public FloodFill(int[][] imagem) {
+        this.imagem = imagem;
+    }
+
+
+
+
+}
---
+++ src/Image.java
@@ -0,0 +1,31 @@
+public class Image {
+    public static int[][] image = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    };
+
+    public void printImage() {
+        for (int[] line : image) {
+            int index = 0;
+            System.out.print("[");
+            for (int pixel : line) {
+                index++;
+                if (index == line.length)
+                    System.out.print(pixel);
+                else
+                    System.out.print(pixel + ", ");
+            }
+            System.out.println("]");
+        }
+        System.out.print("------------------------------");
+    }
+
+}
