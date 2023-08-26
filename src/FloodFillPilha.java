f1944e4a8aa613321e3ed10428fbee88b32b392c
guilherme <guilherme@sisand.com.br>
2023-08-25 21:18:50 -0300

    floodfill w/ stack

--- src/FloodFillPilha.java
+++ src/FloodFillPilha.java
@@ -1,13 +1,80 @@
 public class FloodFillPilha {
-    private int[][] imagem;
-    private int novaCor = 2;
-    private static int corDeFundo = 0;
+    private int[][] matriz;
+    private int largura;
+    private int altura;
+    private int corAntiga;
+    private int novaCor;
 
-    public FloodFillPilha(int[][] imagem) {
-        this.imagem = imagem;
+    public FloodFillPilha(int[][] matriz) {
+        this.matriz = matriz;
+        this.largura = matriz.length;
+        this.altura = matriz[0].length;
     }
 
+    public void floodFill(int x, int y, int novaCor) {
+        corAntiga = matriz[x][y];
+        this.novaCor = novaCor;
 
+        if (corAntiga == novaCor) {
+            return;
+        }
 
+        Pilha<Point> pilha = new Pilha<>();
+        pilha.add(new Point(x, y));
 
+        while (!pilha.isEmpty()) {
+            Point atual = pilha.remove();
+
+            int i = atual.x;
+            int j = atual.y;
+
+            if (matriz[i][j] == corAntiga) {
+                matriz[i][j] = novaCor;
+
+                if (i - 1 >= 0) pilha.add(new Point(i - 1, j));
+                if (i + 1 < largura) pilha.add(new Point(i + 1, j));
+                if (j - 1 >= 0) pilha.add(new Point(i, j - 1));
+                if (j + 1 < altura) pilha.add(new Point(i, j + 1));
+            }
+        }
+    }
+
+    public void imprimirMatriz() {
+        for (int i = 0; i < largura; i++) {
+            for (int j = 0; j < altura; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matriz = {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 7},
+                {1, 1, 1, 1, 1, 0, 0, 0, 7, 0},
+                {0, 0, 1, 1, 1, 0, 0, 7, 0, 0},
+                {0, 0, 0, 1, 1, 0, 7, 0, 0, 0},
+                {0, 0, 0, 1, 1, 7, 0, 0, 0, 0},
+                {0, 0, 0, 1, 7, 0, 0, 0, 0, 0},
+                {0, 0, 0, 7, 1, 0, 0, 0, 0, 0},
+                {0, 0, 7, 1, 1, 0, 0, 0, 0, 0},
+                {0, 7, 0, 1, 1, 0, 0, 0, 0, 0},
+                {7, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+
+        };
+
+        FloodFillPilha floodFillPilha = new FloodFillPilha(matriz);
+
+        System.out.println("Matriz original:");
+        floodFillPilha.imprimirMatriz();
+
+        int x = 0;
+        int y = 0;
+        int novaCor = 2;
+
+        floodFillPilha.floodFill(x, y, novaCor);
+
+        System.out.println("\nMatriz após o flood fill:");
+        floodFillPilha.imprimirMatriz();
+    }
 }
--- src/Pilha.java
+++ src/Pilha.java
@@ -1,28 +1,51 @@
 public class Pilha <T> {
-    private int top = -1;
-    private T[] data;
 
-    Pilha(int size) {
-        data = (T[]) new Object[size];
+    private Object[] elements;
+    private int length;
+    private static final int default_length = 10;
+
+    public Pilha() {
+        this.elements = new Object[default_length];
+        this.length = 0;
     }
 
-    public void add(T data) {
-        this.data[++top] = data;
+    public void add(T element) {
+        if (length == elements.length) {
+            resize();
+        }
+        elements[length] = element;
+        length++;
     }
 
     public T remove() {
-        return this.data[top--];
-    }
-
-    public void clear() {
-        top = -1;
-    }
-
-    public boolean isFull() {
-        return top == data.length - 1;
+        if (isEmpty()) {
+            throw new IllegalStateException("A pilha está vazia");
+        }
+        T element = (T) elements[length - 1];
+        elements[length - 1] = null;
+        length--;
+        return element;
     }
 
     public boolean isEmpty() {
-        return top == -1;
+        return length == 0;
+    }
+
+    private void resize() {
+        int newLength = elements.length * 2;
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(elements, 0, newArray, 0, length);
+        elements = newArray;
+    }
+}
+
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
